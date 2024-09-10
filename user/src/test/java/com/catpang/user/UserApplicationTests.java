da8b63c64cd5ae21bd51a6aa0f45f09f8b3474f1
tellang <tellang@daum.net>
2024-09-11 00:22:51 +0900

    [test]: UserApplicationTests 에서 Eureka 및 Config 에 대한 의존성 제거 및 test 를 위한 yml 및 db 의존성 추가
    Eureka, Config 에 대한 의존성 제거
    test 시 사용할 yml 파일 추가

--- user/src/test/java/com/catpang/user/UserApplicationTests.java
+++ user/src/test/java/com/catpang/user/UserApplicationTests.java
@@ -2,12 +2,17 @@ package com.catpang.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(properties = {
+	"spring.cloud.config.enabled=false",        // Config 서버 비활성화
+	"spring.cloud.netflix.eureka.enabled=false" // Eureka 클라이언트 비활성화
+})
 @SpringBootTest
 class UserApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
