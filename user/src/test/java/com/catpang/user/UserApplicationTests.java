2a5c70c3f7a2856f884650716e1b94e2d7292b27
tellang <tellang@daum.net>
2024-09-12 19:40:47 +0900

    [fix]: 잘못된 어노테이션 매개변수 값 수정
    Eureka 클라이언트 비활성화가 적절히 되지 않던 문제 수정

--- user/src/test/java/com/catpang/user/UserApplicationTests.java
+++ user/src/test/java/com/catpang/user/UserApplicationTests.java
@@ -5,8 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @TestPropertySource(properties = {
-	"spring.cloud.config.enabled=false",        // Config 서버 비활성화
-	"spring.cloud.netflix.eureka.enabled=false" // Eureka 클라이언트 비활성화
+	"spring.cloud.config.enabled=false",    // Config 서버 비활성화
+	"eureka.client.enabled=false"           // Eureka 클라이언트 비활성화
 })
 @SpringBootTest
 class UserApplicationTests {
