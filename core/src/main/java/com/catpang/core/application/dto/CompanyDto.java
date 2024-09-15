6f18560ef881a9965a31965ff4cd75b43e2da28c
tellang <tellang@daum.net>
2024-09-15 23:11:20 +0900

    [feat]: 업체 서비스와의 Feign 클라이언트(FeignCompanyInternalController) 추가
    - `FeignCompanyInternalController` 인터페이스 추가로 업체 서비스와의 통신 지원
    - `company-service`와 Feign을 통해 통신하며, 업체 ID를 기반으로 업체 정보를 조회하는 메서드 구현
    - `getCompany` 메서드를 통해 `CompanyDto.Result` 타입의 업체 정보를 반환
    - `CompanyInternalController`를 상속받아 업체 정보 조회 API 경로(`/companies/{id}`) 매핑

---
+++ core/src/main/java/com/catpang/core/application/dto/CompanyDto.java
@@ -0,0 +1,16 @@
+package com.catpang.core.application.dto;
+
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.With;
+
+public interface CompanyDto {
+
+	@With
+	@Builder
+	record Result(
+		UUID id, Long ownerId, String companyName, String companyAddress, String companyPhone
+	) {
+	}
+}
