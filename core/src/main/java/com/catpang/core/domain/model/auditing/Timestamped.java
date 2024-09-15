58a4e3268f5ed2ed3557bfd891ecaf8ae22fb6d9
tellang <tellang@daum.net>
2024-09-15 15:05:58 +0900

    [feat]: Timestamped 엔티티 추가 및 소프트 삭제 기능 구현
    - 공통 엔티티에 대한 감사 기능을 제공하는 Timestamped 클래스 추가
    - createdAt, updatedAt, deletedAt 등 자동 생성/수정되는 필드 포함
    - softDelete 메서드로 소프트 삭제 처리 및 삭제자 정보 기록

---
+++ core/src/main/java/com/catpang/core/domain/model/auditing/Timestamped.java
@@ -0,0 +1,55 @@
+package com.catpang.core.domain.model.auditing;
+
+import static com.catpang.core.application.service.EntityMapper.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped implements Serializable {
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
+
+	@CreatedBy
+	private Long createdBy;
+
+	@LastModifiedDate
+	@Column
+	private LocalDateTime updatedAt;
+
+	@LastModifiedBy
+	private Long updatedBy;
+
+	@Setter
+	@Column
+	private LocalDateTime deletedAt;
+
+	@Setter
+	private Long deletedBy;
+
+	@Setter
+	private Boolean isDeleted = false;
+
+	public void softDelete() {
+		this.deletedAt = LocalDateTime.now();
+		this.isDeleted = true;
+		this.deletedBy = getUserId(SecurityContextHolder.getContext().getAuthentication().getName());
+	}
+}
