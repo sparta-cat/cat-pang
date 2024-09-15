0cfbdac3b1adad27409ac02bb02caa9f10a1db65
tellang <tellang@daum.net>
2024-09-15 22:55:34 +0900

    [feat]: 주문 서비스 및 매퍼 추가
    - `OrderService` 클래스 생성하여 주문 생성, 조회, 수정, 삭제 등의 비즈니스 로직 구현
    - `OrderMapper` 클래스 추가로 DTO와 엔티티 간의 변환 책임 분리

---
+++ order/src/main/java/com/catpang/order/application/service/OrderMapper.java
@@ -0,0 +1,44 @@
+package com.catpang.order.application.service;
+
+import static com.catpang.core.application.dto.OrderDto.*;
+
+import org.springframework.data.domain.Page;
+
+import com.catpang.core.application.service.EntityMapper;
+import com.catpang.order.domain.model.Order;
+
+public class OrderMapper extends EntityMapper {
+	public static Order entityFrom(Create createDto) {
+		return Order.builder()
+			.companyId(createDto.companyId())
+			.totalQuantity(createDto.totalQuantity())
+			.ownerId(createDto.ownerId())
+			.build();
+	}
+
+	public static Result dtoFrom(Order order) {
+		return Result.builder()
+			.id(order.getId())
+			.companyId(order.getCompanyId())
+			.totalQuantity(order.getTotalQuantity())
+			.ownerId(order.getOwnerId())
+			.build();
+	}
+
+	public static Delete.Result dtoWithDeleter(Order order, Long deleterId) {
+		return Delete.Result.builder()
+			.id(order.getId())
+			.deleterId(deleterId)
+			.deletedAt(order.getDeletedAt())
+			.build();
+	}
+
+	public static Page<Result> dtoFrom(Page<Order> orders) {
+		return orders.map(OrderMapper::dtoFrom);
+	}
+
+	public static Order putToEntity(Put putDto, Order order) {
+		order.setTotalQuantity(putDto.totalQuantity());
+		return order;
+	}
+}
