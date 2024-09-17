0c908a03255c19f7c68c8cf9578f1d8727e8ea01
tellang <tellang@daum.net>
2024-09-18 00:19:10 +0900

    [fix]: Order 생성시 id 가 들어가던 builder 수정

--- order/src/main/java/com/catpang/order/domain/model/Order.java
+++ order/src/main/java/com/catpang/order/domain/model/Order.java
@@ -17,9 +17,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.With;
 
+@With
 @Getter
-@Builder
 @Entity
 @Table(name = "p_orders")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -42,6 +43,12 @@ public class Order extends Timestamped {
 
 	@NotNull
 	private Long ownerId;
+
+	@Builder
+	public Order(UUID companyId, Long ownerId) {
+		this.companyId = companyId;
+		this.ownerId = ownerId;
+	}
 }
 
 
