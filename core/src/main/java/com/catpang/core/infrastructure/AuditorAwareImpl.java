3bf3eb79b1ff1c6da06f63a741befb7d583eb6ab
macdduck <knh9612@naver.com>
2024-09-20 01:11:54 +0900

    [feat]: 사용자 생성 시 Timestamped 기능하도록 코드 추가

--- core/src/main/java/com/catpang/core/infrastructure/AuditorAwareImpl.java
+++ core/src/main/java/com/catpang/core/infrastructure/AuditorAwareImpl.java
@@ -1,25 +1,24 @@
 package com.catpang.core.infrastructure;
 
-import java.util.Optional;
-
+import com.catpang.core.application.service.EntityMapper;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.catpang.core.application.service.EntityMapper;
+import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<Long> {
 
-	@Override
-	public Optional<Long> getCurrentAuditor() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-		if (authentication == null || !authentication.isAuthenticated()) {
-			return Optional.empty();
-		}
+        if (authentication == null || !authentication.isAuthenticated() || "anonymousUser".equals(authentication.getName())) {
+            return Optional.empty();
+        }
 
-		// 사용자 ID를 반환
-		return Optional.of(EntityMapper.getUserId(((UserDetails)authentication.getPrincipal()).getUsername()));
-	}
+        // 사용자 ID를 반환
+        return Optional.of(EntityMapper.getUserId(((UserDetails) (authentication.getPrincipal())).getUsername()));
+    }
 }
