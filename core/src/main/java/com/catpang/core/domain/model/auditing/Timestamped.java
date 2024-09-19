998e1ff1187ea165139eb0f2249141f89eb884ab
macdduck <knh9612@naver.com>
2024-09-20 01:02:52 +0900

    [feat]: User엔티티 저장 시 Timestamp 작동되도록 설정

--- core/src/main/java/com/catpang/core/domain/model/auditing/Timestamped.java
+++ core/src/main/java/com/catpang/core/domain/model/auditing/Timestamped.java
@@ -1,10 +1,10 @@
 package com.catpang.core.domain.model.auditing;
 
-import static com.catpang.core.application.service.EntityMapper.*;
-
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -12,11 +12,10 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import lombok.Setter;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import static com.catpang.core.application.service.EntityMapper.getUserId;
 
 @Getter
 @MappedSuperclass
@@ -51,5 +50,10 @@ public abstract class Timestamped implements Serializable {
 		this.deletedAt = LocalDateTime.now();
 		this.isDeleted = true;
 		this.deletedBy = getUserId(SecurityContextHolder.getContext().getAuthentication().getName());
+	}
+
+	public void setCreatedBy(String creater) {
+		this.createdBy = Long.valueOf(creater);
+		this.updatedBy = Long.valueOf(creater);
 	}
 }
