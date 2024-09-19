54c72712b0e95d2e854ff7cc9f9f04260ef14425
macdduck <knh9612@naver.com>
2024-09-20 00:53:15 +0900

    [remove]: 패키지 구조 변경으로 인한 불필요한 파일 삭제

---
+++ user/src/main/java/com/catpang/user/domain/repository/CompanyDeliveryManagerRepository.java
@@ -0,0 +1,7 @@
+package com.catpang.user.domain.repository;
+
+import com.catpang.user.domain.model.CompanyDeliveryManager;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyDeliveryManagerRepository extends JpaRepository<CompanyDeliveryManager, Long> {
+}
