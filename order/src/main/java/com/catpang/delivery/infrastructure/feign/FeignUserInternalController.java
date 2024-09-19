fd070f17b46aa6e3585b6959b61d1d225c2f81e1
tellang <tellang@daum.net>
2024-09-20 02:42:11 +0900

    [fix]: User 요구사항에 따른 변경 적용

--- order/src/main/java/com/catpang/delivery/infrastructure/feign/FeignUserInternalController.java
+++ order/src/main/java/com/catpang/delivery/infrastructure/feign/FeignUserInternalController.java
@@ -5,13 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.catpang.core.application.dto.UserDto;
-import com.catpang.core.application.response.ApiResponse;
 import com.catpang.core.presentation.controller.UserInternalController;
 
-@FeignClient(name = "user-service")
+@FeignClient(name = "user")
 public interface FeignUserInternalController extends UserInternalController {
 
 	@GetMapping("/api/v1/internal/users/{id}")
-	ApiResponse<UserDto.Result> getUser(@PathVariable Long id);
+	UserDto.Result getUser(@PathVariable Long id);
 }
 
